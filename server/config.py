8882f09f29d990760240e1e6b19954738f421dc1
fauzdar <jat.abhishek.99@gmail.com>
2025-06-16 06:30:42 +0530

    Switched to JSON-based auth and removed DB

--- server/config.py
+++ server/config.py
@@ -1,8 +1,5 @@
 # config.py
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'db.sqlite3')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
